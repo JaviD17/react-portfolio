f637c24a1d11aa2c2e7b8c6e63b5cbf51877abea
Javier Dominguez II <Javi7057@gmail.com>
2021-10-07 17:28:05 -0500

    fixed issue in SingleProject where pictures were not loading'

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -3,36 +3,42 @@ import SingleProject from "../SingleProject";
 // import deepThoughts from "../../utils/photos/deep-thoughts.jpg";
 // import reactPortfolio from "../../utils/photos/react-portfolio.jpg";
 
+
 const Portfolio = () => {
   const [categories] = useState([
     {
       title: "React Portfolio",
       alt: "Portofolio made in react",
-      src: "/images/react-portfolio.jpg",
+      // src: "/images/react-portfolio.jpg",
+      src: "react-portfolio",
       link: "https://github.com/JaviD17/react-portfolio",
     },
     {
       title: "Deep Thoughts",
       alt: "Social media application",
-      src: "/images/deep-thoughts.jpg",
+      // src: "/images/deep-thoughts.jpg",
+      src: "deep-thoughts",
       link: "https://github.com/JaviD17/deep-thoughts",
     },
     {
       title: "Photo Port",
       alt: "React application",
-      src: "/images/react-application.jpg",
+      // src: "/images/react-application.jpg",
+      src: "photo-port",
       link: "https://javid17.github.io/photo-port/",
     },
     {
         title: "Social Network API",
         alt: "Social Network",
-        src: "/images/social-network-api.jpg",
+        // src: "/images/social-network-api.jpg",
+        src: "social-network-api",
         link: "https://github.com/JaviD17/social-network-api"
     },
     {
         title: "Regex Tutorial",
         alt: "Regex",
-        src: "/images/regex.jpg",
+        // src: "/images/regex.jpg",
+        src: "regex-tutorial",
         link: "https://github.com/JaviD17/regex-tutorial"
     }
   ]);
--- client/src/components/SingleProject/index.js
+++ client/src/components/SingleProject/index.js
@@ -1,21 +1,35 @@
 import React from "react";
-// import deepThoughts from "../../assets/photos/deep-thoughts.jpg";
-// import reactPortfolio from '../../assets/photos/react-portfolio.jpg';
-// import reactApplication from '../../utils/photos/react-application';
+import deepThoughts from "../../assets/photos/deep-thoughts.jpg";
+import reactPortfolio from "../../assets/photos/react-portfolio.jpg";
+import reactApplication from "../../assets/photos/react-application.jpg";
+import socialNetwork from "../../assets/photos/social-network-api.jpg";
+import regexTutorial from "../../assets/photos/regex.jpg";
 
 const SingleProject = ({ categories, index }) => {
-//   console.log(categories);
+  //   console.log(categories);
+  // src={`${categories[index].src}`}
 
   return (
     <div className="single-project-container">
       <div className="single-project-card">
-        <img src={`${categories[index].src}`} alt={`${categories[index].alt}`} />
+        {categories[index].src === "react-portfolio" && (
+          <img src={reactPortfolio} alt={`${categories[index].alt}`} />
+        )}
+        {categories[index].src === "deep-thoughts" && (
+          <img src={deepThoughts} alt={`${categories[index].alt}`} />
+        )}
+        {categories[index].src === "photo-port" && (
+          <img src={reactApplication} alt={`${categories[index].alt}`} />
+        )}
+        {categories[index].src === "social-network-api" && (
+          <img src={socialNetwork} alt={`${categories[index].alt}`} />
+        )}
+        {categories[index].src === "regex-tutorial" && (
+          <img src={regexTutorial} alt={`${categories[index].alt}`} />
+        )}
         <div className="single-project-card-details">
           <p className="single-project-caption">{categories[index].title}</p>
-          <a
-            className="single-project-link"
-            href={`${categories[index].link}`}
-          >
+          <a className="single-project-link" href={`${categories[index].link}`}>
             View Project
           </a>
         </div>
